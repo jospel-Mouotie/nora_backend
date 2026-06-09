6dc8657b9a176bc95960ad45fb090fe3ad2ef76d
unknown <mouotiej@gmail.com>
2026-06-09 13:03:05 +0200

    Initial commit : Structure propre de Nora-v22

---
+++ docker/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Attendre que la base de données MySQL distante réponde
+sleep 3
+
+echo "Exécution des migrations Laravel..."
+php artisan migrate --force
+
+echo "Optimisation des configurations de l'API..."
+php artisan config:cache
+php artisan route:cache
+
+# Démarrer PHP-FPM en arrière-plan
+php-fpm -D
+
+echo "Démarrage de Nginx pour l'API..."
+nginx -g "daemon off;"
